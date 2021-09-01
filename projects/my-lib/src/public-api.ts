8a5be94fa998e71bcd31820fb291612b602ee201
VANNAL <Vannal4500@gmail.com>
2021-09-01 16:39:39 +0700

    export all services in product

--- projects/my-lib/src/public-api.ts
+++ projects/my-lib/src/public-api.ts
@@ -7,3 +7,6 @@
 // export * from './lib/my-lib.module';
 export * from './product/item.module';
 export * from './product/product.routes';
+export * from './product/unit/unit.service';
+export * from './product/item/item.service';
+export * from './product/item-type/item-type.service';
